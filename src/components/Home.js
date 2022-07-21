b1989aab7dcd37fab3202be5aaa050cfb1877610
Blaster4385 <venkateshchaturvedi12@gmail.com>
2022-07-21 10:58:41 +0530

    Fix text color

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,8 +9,8 @@ const Home = () => {
     return (
         <Transition>
             <div>
-                <div className="sm:text-5xl font-medium font-mono text-slate-800 dark:text-slate-300">Welcome to PixelBlaster-OS</div>
-                <div className="sm:text-5xl font-medium font-mono text-slate-800 dark:text-slate-300 type-anim" style={{ width: "100%" }}>Are You Ready to Explode Your Device?</div>
+                <div className="sm:text-5xl font-medium font-mono text-slate-700 dark:text-slate-300">Welcome to PixelBlaster-OS</div>
+                <div className="sm:text-5xl font-medium font-mono text-slate-700 dark:text-slate-300 type-anim" style={{ width: "100%" }}>Are You Ready to Explode Your Device?</div>
                 <br></br>
                 <button className="bg-slate-700 dark:bg-slate-300 hover:bg-slate-900 dark:hover:bg-slate-500 text-slate-300 dark:text-slate-700 font-bold py-2 px-4 border border-slate-700  rounded-xl sm:text-3xl"
                     onClick={() => navigate("/about")}>
