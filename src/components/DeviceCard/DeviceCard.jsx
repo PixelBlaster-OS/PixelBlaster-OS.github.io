4e0727bb785d3a30a3d451649e3411bdccf42429
ankitgupta-13 <guptankit0522@gmail.com>
2023-07-17 22:36:20 +0530

    Responsive design

--- src/components/DeviceCard/DeviceCard.jsx
+++ src/components/DeviceCard/DeviceCard.jsx
@@ -1,18 +1,22 @@
-import React from 'react'
-import styles from './DeviceCard.module.css'
+import React from "react";
+import styles from "./DeviceCard.module.css";
 
 const DeviceCard = ({ data, key }) => {
-    const { devicename, maintainer } = data;
-    return (
-        <div className={styles.card} key={key}>
-            <img className={styles.card__image} src="/assets/images/left-hole.png" alt="device" />
-            <div className={styles.card__content}>
-                <p className={styles.card__content__title}>{devicename}</p>
-                <p className={styles.card__content__text}>By {maintainer}</p>
-            </div>
-            <button className={styles.card__button}>Download</button>
-        </div>
-    )
-}
+  const { devicename, maintainer } = data;
+  return (
+    <div className={styles.card} key={key}>
+      <img
+        className={styles.card__image}
+        src="/assets/images/left-hole.png"
+        alt="device"
+      />
+      <div className={styles.card__content}>
+        <p className={styles.card__content__title}>{devicename}</p>
+        <p className={styles.card__content__text}>By {maintainer}</p>
+      </div>
+      <button className={styles.card__button}>Download</button>
+    </div>
+  );
+};
 
-export default DeviceCard
+export default DeviceCard;
--- src/pages/Downloads/Downloads.jsx
+++ src/pages/Downloads/Downloads.jsx
@@ -4,23 +4,23 @@ import DeviceCard from '../../components/DeviceCard/DeviceCard'
 
 const Downloads = () => {
 
-    const url = 'https://raw.githubusercontent.com/PixelBlaster-Releases/website_api/main/devices.json';
-    const [post, setPost] = React.useState([]);
+  const url = 'https://raw.githubusercontent.com/PixelBlaster-Releases/website_api/main/devices.json';
+  const [post, setPost] = React.useState([]);
 
-    React.useEffect(() => {
-        fetch(url)
-          .then(res => res.json())
-          .then(data => {
-            setPost(data.response)
-            console.log(data)
-          })
-          .catch(err => console.log(err));
-      }, []);
-    return (
-        <div className={styles.card}>
-        {post.length > 0 ? post.map(post => <DeviceCard key={post.id} data={post} />) : <div >No devices found.</div>}
-        </div>
-    )
+  React.useEffect(() => {
+    fetch(url)
+      .then(res => res.json())
+      .then(data => {
+        setPost(data.response)
+        console.log(data)
+      })
+      .catch(err => console.log(err));
+  }, []);
+  return (
+    <div className={styles.card}>
+      {post.length > 0 ? post.map(post => <DeviceCard key={post.id} data={post} />) : <div >No devices found.</div>}
+    </div>
+  )
 }
 
 export default Downloads
